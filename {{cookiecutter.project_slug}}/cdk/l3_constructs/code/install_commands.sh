34c8f4475c163a6a81bd6ca3379ff92b2a88c7a8
Ahmed Elgamal <elgamala@amazon.de>
2021-06-10 02:47:07 +0200

    keep in the working dir while installing the requirements

--- {{cookiecutter.project_slug}}/cdk/l3_constructs/code/install_commands.sh
+++ {{cookiecutter.project_slug}}/cdk/l3_constructs/code/install_commands.sh
@@ -5,8 +5,6 @@ apt-get install jq -y
 
 aws sts get-caller-identity
 
-cd ../..
-
 pip install -r requirements.txt --cache-dir=/root/cache/pip
 npm config set cache=/root/cache/npm
 npm install -g aws-cdk
